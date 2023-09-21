363f8c56977536287c205f2ae1f71044a454d7f2
azzudin1108 <82307614+azzudin1108@users.noreply.github.com>
2023-09-21 20:13:51 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,35 @@
+const img = document.querySelectorAll('#project .carousel .carousel-img .img-project img');
+const btn = document.querySelectorAll('#project .carousel-btn');
+
+const gameRev = img[0];
+const next = btn[1];
+const prev = btn[0];
+
+next.addEventListener('click', function (e) {
+    if (gameRev.classList.contains('slide-2') == false) {
+        if (gameRev.classList.contains('slide-3') == false) {
+            gameRev.classList.add('slide-2');
+            gameRev.classList.remove('slide-3');
+            gameRev.classList.remove('slide-1');
+        }
+    } else {
+        gameRev.classList.add('slide-3');
+        gameRev.classList.remove('slide-2');
+        gameRev.classList.remove('slide-1');
+    }
+    e.preventDefault();
+});
+
+
+prev.addEventListener('click', function (e) {
+    if (gameRev.classList.contains('slide-2') == true) {
+        gameRev.classList.add('slide-1');
+        gameRev.classList.remove('slide-2');
+        gameRev.classList.remove('slide-3');
+    } else if (gameRev.classList.contains('slide-3') == true) {
+        gameRev.classList.add('slide-2');
+        gameRev.classList.remove('slide-1');
+        gameRev.classList.remove('slide-3');
+    }
+    e.preventDefault();
+});
