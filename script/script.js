a8628ce7a688844dd7e474810a31877678997349
azzudin1108 <82307614+azzudin1108@users.noreply.github.com>
2023-09-22 13:00:46 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -11,25 +11,28 @@ next.addEventListener('click', function (e) {
             gameRev.classList.add('slide-2');
             gameRev.classList.remove('slide-3');
             gameRev.classList.remove('slide-1');
+            gameRev.parentElement.setAttribute('href', 'https://azzudin1108.github.io/portfolio/');
         }
     } else {
         gameRev.classList.add('slide-3');
         gameRev.classList.remove('slide-2');
         gameRev.classList.remove('slide-1');
+        gameRev.parentElement.setAttribute('href', 'https://azzudin1108.github.io/invitation/');
     }
     e.preventDefault();
 });
-
 
 prev.addEventListener('click', function (e) {
     if (gameRev.classList.contains('slide-2') == true) {
         gameRev.classList.add('slide-1');
         gameRev.classList.remove('slide-2');
         gameRev.classList.remove('slide-3');
+        gameRev.parentElement.setAttribute('href', 'https://azzudin1108.github.io/game/');
     } else if (gameRev.classList.contains('slide-3') == true) {
         gameRev.classList.add('slide-2');
         gameRev.classList.remove('slide-1');
         gameRev.classList.remove('slide-3');
+        gameRev.parentElement.setAttribute('href', 'https://azzudin1108.github.io/portfolio/');
     }
     e.preventDefault();
 });
